cd96e1a92129cdf7393420d3c1516418bef1f506
RosDeeper <hoanggg237@gmail.com>
2024-06-11 08:40:21 +0700

    home_page UI bugs

--- lib/presentation/home/home.page.dart
+++ lib/presentation/home/home.page.dart
@@ -10,62 +10,63 @@ import 'components/post_list.dart';
 import 'components/topic_tab_bar.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+    const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
+    @override
+    State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const SliverToBoxAdapter(child: SizedBox(height: 32)),
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 24,
-              right: 8,
-            ),
-            child: Row(
-              children: [
-                Text(
-                  "Home",
-                  style: textLargeTitle.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
+    @override
+    Widget build(BuildContext context) {
+        return CustomScrollView(
+            slivers: [
+                const SliverToBoxAdapter(child: SizedBox(height: 32)),
+                SliverToBoxAdapter(
+                    child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 24,
+                            right: 8,
+                        ),
+                        child: Row(
+                            children: [
+                                Text(
+                                    "Home",
+                                    style: textLargeTitle.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                    ),
+                                ),
+                                const Spacer(),
+                                IconButton(
+                                    onPressed: () => context.push(Routes.CREATE_POST),
+                                    icon: const Icon(
+                                        Icons.create_outlined,
+                                        size: 24,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
                 ),
-                const Spacer(),
-                IconButton(
-                  onPressed: () => context.push(Routes.CREATE_POST),
-                  icon: const Icon(
-                    Icons.create_outlined,
-                    size: 24,
-                  ),
+
+                const SliverToBoxAdapter(child: SizedBox(height: 24)),
+                SliverStickyHeader(
+                    header: const TopicTabBar(),
+                    sliver: SliverToBoxAdapter(
+                        child: Consumer(builder: (context, ref, child) {
+                            final topicId = ref.watch(currentPostTopicStateProvider);
+                            if (topicId == null) {
+                                return const PostList();
+                            }
+                            return PostList(
+                                key: ValueKey(topicId),
+                                topicId: topicId,
+                                //return postList
+                            );
+                        }),
+                    ),
                 ),
-              ],
-            ),
-          ),
-        ),
-        const SliverToBoxAdapter(child: SizedBox(height: 24)),
-        SliverStickyHeader(
-          header: const TopicTabBar(),
-          sliver: SliverToBoxAdapter(
-            child: Consumer(builder: (context, ref, child) {
-              final topicId = ref.watch(currentPostTopicStateProvider);
-              if (topicId == null) {
-                return const PostList();
-              }
-              return PostList(
-                key: ValueKey(topicId),
-                topicId: topicId,
-                //return postList
-              );
-            }),
-          ),
-        ),
-      ],
-    );
-  }
+            ],
+        );
+    }
 }
